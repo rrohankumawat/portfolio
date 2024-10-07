5638ea91450aca9241041608797db7041420e356
Rohan Kumawat <108892371+rohanpin1@users.noreply.github.com>
2024-10-08 00:30:52 +0530

    Managed Code

---
+++ js/content.js
@@ -0,0 +1,13 @@
+import { LeftPaneSkills } from "../store/store.js";
+import { RightPaneSkill } from "../store/store.js";
+var row = document.querySelector('.skills_leftrow')
+
+for(let x of LeftPaneSkills){
+    row.innerHTML +=`<div class="skill mb-4"> <div class="d-flex justify-content-between"> <p class="mb-2">${x.skillName}</p> <p class="mb-2">${x.skillVal}%</p> </div> <div class="progress"> <div class="progress-bar bg-${x.color}" role="progressbar" aria-valuenow="${x.skillVal}" aria-valuemin="0" aria-valuemax="100"></div> </div> </div>`
+}
+
+var rightRow = document.querySelector('.skills_rightrow');
+
+for(let i of RightPaneSkill){
+    rightRow.innerHTML += `<div class="skill mb-4"> <div class="d-flex justify-content-between"> <p class="mb-2">${i.skillName}</p> <p class="mb-2">${i.skillVal}%</p> </div> <div class="progress"> <div class="progress-bar bg-${i.color}" role="progressbar" aria-valuenow="${i.skillVal}" aria-valuemin="0" aria-valuemax="100"></div> </div> </div>`
+}
---
+++ store/store.js
@@ -0,0 +1,13 @@
+export const LeftPaneSkills = [
+    {skillName : 'ASP.NET Core', skillVal : '95', color:'primary'},
+    {skillName : 'ASP.NET MVC', skillVal : '85', color:'warning'},
+    {skillName : 'Web APIs', skillVal : '90', color:'danger'},
+    
+]
+
+export const RightPaneSkill =[
+    {skillName : 'SQL / MongoDb', skillVal : '90', color:'danger'},
+    {skillName : 'HTML / CSS / JavaScript / React', skillVal : '95', color:'primary'},
+    {skillName : 'Microservices', skillVal : '85', color:'info'},
+    
+]
